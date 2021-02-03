91ea4be940052e606cb630052d87e79d5071fd1b
lubna40 <lubu7035@gmail.com>
2021-02-03 23:41:43 +0600

    Update port number

--- config/config.js
+++ config/config.js
@@ -3,6 +3,7 @@ const env = process.env;
 const config = {
   mongoURI: env.MONGO_URI,
   mongoPORT: env.MONGO_PORT,
-  jwtSecret: env.JWT_SECRET
+  jwtSecret: env.JWT_SECRET,
+  PORT: env.PORT
 };
 module.exports = config;
