c4b6c7596938d03d87d1de8f3ac9fc799957a53b
Ovishek Paul <ovishek@kinetik.care>
2021-02-05 20:17:48 +0600

    added course and exam page

---
+++ src/components/Courses/components/EnrollmentRequest.js
@@ -0,0 +1,77 @@
+import Search from "antd/lib/input/Search";
+import styled from "styled-components";
+import _ from 'underscore';
+import { Popconfirm } from "antd";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck, faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+
+const SearchStyled = styled(Search)`
+  width: 100%;
+`;
+
+const Container = styled.div`
+  overflow: auto;
+`;
+
+const HeaderLabel = styled.div`
+  color: grey;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  height: 30px;
+  font-size: 12px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+
+const Row = styled.div`
+  display: grid;
+  grid-gap: 10px;
+  grid-template-columns: ${props => props.columns || 'auto'};
+`;
+
+const FontAwesomeIconWrapper = styled(FontAwesomeIcon)`
+  cursor: pointer;
+  margin: auto;
+  margin-left: 5px;
+  width: 15px;
+  height: 15px;
+`;
+
+
+const getName = obj => `${obj.firstName} ${obj.lastName}`
+const Card = ({ student }) => {
+  return (
+    <Row columns="repeat(2, 1fr) 20px">
+      <Wrapper>{getName(student)}</Wrapper>
+      <Wrapper>{student.registrationNo}</Wrapper>
+      <Wrapper>
+      <FontAwesomeIconWrapper
+        icon={faCheckCircle}
+        color="green"
+      />
+      </Wrapper>
+    </Row>
+  );
+};
+
+const EnrollmentRequest = ({
+  students
+}) => {
+  return (
+    <Container>
+      <SearchStyled placeholder="Search" />
+      <Row columns="repeat(2, 1fr) 20px">
+        <HeaderLabel>Name</HeaderLabel>
+        <HeaderLabel>Regi No.</HeaderLabel>
+        <HeaderLabel></HeaderLabel>
+      </Row>
+      {_.map(students, student => <Card student={student} />)}
+    </Container>
+  );
+};
+
+export default EnrollmentRequest;
