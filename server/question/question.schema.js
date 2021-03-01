3ad035610373d6ffe34e4aa54f46f8d2de80bce9
Ovishek Paul <ovishek.private@gmail.com>
2021-03-01 11:49:17 +0600

    add fields to support matching questions

--- server/question/question.schema.js
+++ server/question/question.schema.js
@@ -1,35 +1,63 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
+const uniqid = require("uniqid");
 const { Schema } = mongoose;
-const departmentSchema = require('../department/department.schema');
+const departmentSchema = require("../department/department.schema");
 
-const questionSchema = new Schema({
+const questionSchema = new Schema(
+  {
     title: { type: String, required: true },
     marks: { type: Number, required: true },
-    authorID: { type: Schema.Types.ObjectId, required: true, ref: 'Teacher' },
+    authorID: { type: Schema.Types.ObjectId, required: true, ref: "Teacher" },
     type: {
-        type: String, // MCQM, MCQS, BROAD, PROGQUE
-        required: true
+      type: String, // mcq, broad, codingQuestion, quiz, trueFalse
+      required: true,
     },
-    options: [{
+    options: [
+      {
         value: String,
-        isAnswer: { type: Boolean, required: true }
-    }],
-    answer: String,
+        isAnswer: { type: Boolean, required: true },
+      },
+    ],
+    matchingOptions: {
+      leftSide: [
+        {
+          id: {
+            type: String,
+            default: uniqid("match"),
+            required: true,
+            unique: true,
+          },
+          value: String,
+          matchingID: String,
+        },
+      ],
+      rightSide: [
+        {
+          id: {
+            type: String,
+            default: uniqid("match"),
+            required: true,
+            unique: true,
+          },
+          value: String,
+        },
+      ],
+    },
     body: String,
     defaultCode: String,
-    securityType: { type: String, default: 'public', required: true },
+    securityType: { type: String, default: "public", required: true },
     teacherAccess: [Schema.Types.ObjectId],
     department: {
-        type: departmentSchema,
-        required: true
+      type: departmentSchema,
+      required: true,
     },
-},
-{
-  timestamps: true,
-  toObject: { getters: true, virtuals: true },
-  toJSON: { getters: true, virtuals: true },
-  strict: true
-});
+  },
+  {
+    timestamps: true,
+    toObject: { getters: true, virtuals: true },
+    toJSON: { getters: true, virtuals: true },
+    strict: true,
+  }
+);
 
 module.exports = questionSchema;
