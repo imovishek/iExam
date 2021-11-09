28d2e19ba759c460072e0bbc59eef02ad385cbd0
royantar0311 <royantar1@gmail.com>
2021-11-09 15:00:20 +0600

    add sengrid

--- backend/server/email/email.helper.js
+++ backend/server/email/email.helper.js
@@ -1,41 +1,17 @@
-const nodemailer = require("nodemailer");
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
 exports.sendMail = async (to, subject, body) => {
-  // sending email is currently off
-  // until we get a better solution
-  return;
-  // let testAccount = await nodemailer.createTestAccount();
-
-  // // create reusable transporter object using the default SMTP transport
-  // let transporter = nodemailer.createTransport({
-  //   host: "smtp.gmail.com",
-  //   port: 587,
-  //   secure: false, // true for 465, false for other ports
-  //   auth: {
-  //     user: process.env.GMAIL_ACCOUNT, // generated ethereal user
-  //     pass: process.env.GMAIL_PASSWORD, // generated ethereal password
-  //     //user: testAccount.user, // generated ethereal user
-  //     //pass: testAccount.pass, // generated ethereal password
-  //   },
-  // });
-  // console.log(
-  //   process.env.GMAIL_ACCOUNT,
-  //   process.env.GMAIL_PASSWORD,
-  //   testAccount
-  // );
-  // // send mail with defined transport object
-  // let info = await transporter.sendMail({
-  //   from: process.env.GMAIL_ACCOUNT, // sender address
-  //   to, // list of receivers
-  //   subject, // Subject line
-  //   html: body, // html body
-  // });
-
-  // console.log("Message sent: %s", info.messageId);
-  // // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-  // // Preview only available when sending through an Ethereal account
-  // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  // // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+  const msg = {
+    from: process.env.SENGRID_SENDER_EMAIL, // Change to your recipient
+    to,
+    subject,
+    text: body,
+  };
+  sgMail
+    .send(msg)
+    .then(() => console.log("sent"))
+    .catch((err) => console.log({ err, body: err.response.body.errors }));
 };
 
 exports.generateRegisterEmailBody = (name, password) => `
