49ff6dcc7097c01e8d4f8c2960c02465c65a72a6
royantar0311 <royantar1@gmail.com>
2021-12-06 13:32:01 +0600

    remove autosave && add ecosystem

--- frontend/src/components/Exams/Student/ExamPage.js
+++ frontend/src/components/Exams/Student/ExamPage.js
@@ -178,24 +178,24 @@ const ExamPage = ({ dispatch, user, hasBack = true }) => {
   };
 
   const submitSilentPaperHandler = async () => {
-    const showingPaper = window.location.pathname.match(/\/answer$/);
-    if (!showingPaper) return;
-    const cleanPaper = {
-      ...virtualState.paper,
-    };
-    console.log("virtualState.paper", cleanPaper, id);
-    try {
-      await api.updateExamPaperForStudent(id, cleanPaper);
-      await updateExamOnUI();
-      setSavedText("Saved a few seconds ago");
-    } catch (err) {
-      message.error(err.response.data.message);
-      dispatch(push(`/exam/${id}`));
-      try {
-        await updateExamOnUI();
-      } catch (error) {}
-      console.log(err);
-    }
+  //   const showingPaper = window.location.pathname.match(/\/answer$/);
+  //   if (!showingPaper) return;
+  //   const cleanPaper = {
+  //     ...virtualState.paper,
+  //   };
+  //   console.log("virtualState.paper", cleanPaper, id);
+  //   try {
+  //     await api.updateExamPaperForStudent(id, cleanPaper);
+  //     await updateExamOnUI();
+  //     setSavedText("Saved a few seconds ago");
+  //   } catch (err) {
+  //     message.error(err.response.data.message);
+  //     dispatch(push(`/exam/${id}`));
+  //     try {
+  //       await updateExamOnUI();
+  //     } catch (error) {}
+  //     console.log(err);
+  //   }
   };
 
   const autoSubmitUpdateHandler = async (checked) => {
@@ -207,27 +207,27 @@ const ExamPage = ({ dispatch, user, hasBack = true }) => {
     setSwitchLoading(false);
   };
   const AUTO_SAVE_TIME_INTERVAL = 5000;
-  useEffect(() => {
-    if (user.autoSubmitPaper) {
-      // console.log('Starting new one...........');
-      const interval = setInterval(async () => {
-        if (
-          virtualState.exam &&
-          (getExamStatus(virtualState.exam) === "ended" ||
-            meGotBanned(virtualState.exam, user))
-        ) {
-          setSavedText("");
-          return clearInterval(interval);
-        }
-        setSavedText("Saving...");
-        await submitSilentPaperHandler();
-      }, AUTO_SAVE_TIME_INTERVAL);
-      return () => {
-        // console.log('Killing prev one........');
-        clearInterval(interval);
-      };
-    }
-  }, [user.autoSubmitPaper]);
+  // useEffect(() => {
+  //   if (user.autoSubmitPaper) {
+  //     // console.log('Starting new one...........');
+  //     const interval = setInterval(async () => {
+  //       if (
+  //         virtualState.exam &&
+  //         (getExamStatus(virtualState.exam) === "ended" ||
+  //           meGotBanned(virtualState.exam, user))
+  //       ) {
+  //         setSavedText("");
+  //         return clearInterval(interval);
+  //       }
+  //       setSavedText("Saving...");
+  //       await submitSilentPaperHandler();
+  //     }, AUTO_SAVE_TIME_INTERVAL);
+  //     return () => {
+  //       // console.log('Killing prev one........');
+  //       clearInterval(interval);
+  //     };
+  //   }
+  // }, [user.autoSubmitPaper]);
   const isDisabled =
     getExamStatus(exam) !== "running" || meGotBanned(exam, user) || isLoading;
   return (
@@ -256,13 +256,13 @@ const ExamPage = ({ dispatch, user, hasBack = true }) => {
                       <span style={{ marginRight: "10px" }}>Auto submit: </span>
                       <Switch
                         loading={switchLoading}
-                        disabled={isDisabled}
-                        checked={user.autoSubmitPaper}
+                        disabled={true}
+                        checked={false}
                         onChange={autoSubmitUpdateHandler}
                         style={{ marginRight: "10px" }}
                       />
                     </div>
-                    {user.autoSubmitPaper && <div>{savedText}</div>}
+                    {user.autoSubmitPaper && <div>{}</div>}
                   </Col>
 
                   <ButtonStyled
